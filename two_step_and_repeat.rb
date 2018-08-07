997889243b9825e3e6acb90159ffdf8588f66a17
seamus-quinn <sumaesquinn@gmail.com>
2018-08-07 21:26:30 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -3,4 +3,13 @@
 
 def first_steps
   loop do
-    
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Left foot back"
+    sleep(0.5)
+    puts "Right foot back"
+    sleep(0.5)
+    puts "Stop"
+    sleep(1)
+  end 
+end 
